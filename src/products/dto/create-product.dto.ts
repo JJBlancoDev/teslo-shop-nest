b1a54c3bbb1433792ca3436ee872e50956ad833f
JJBlancoDev <janerjoseblancomartinez@gmail.com>
2025-04-19 22:46:17 -0500

    Actualizando dto, añadiendo tags

--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -37,6 +37,13 @@ export class CreateProductDto {
   @IsArray()
   sizes: string[];
 
+  @IsOptional()
+  @IsString({
+    each: true,
+  })
+  @IsArray()
+  tags?: string[];
+
   @IsIn(['men', 'women', 'kid', 'unisex'])
   gender: string;
 }
