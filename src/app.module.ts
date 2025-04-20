4eed625baf23f230cf3ee0fecaf6a1a468f5b2f4
JJBlancoDev <janerjoseblancomartinez@gmail.com>
2025-04-19 19:27:42 -0500

    Configurando conexion postgres typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,21 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT!,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      autoLoadEntities: true, // Cargue automatico de entidades
+      synchronize: true, // Cuando se hace un cambio en las entidades las sincroniza automaticamente
+    }),
+  ],
   controllers: [],
   providers: [],
 })
