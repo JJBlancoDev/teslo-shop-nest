31bb463310fe19c146eaa3a2a5906796e07b63ba
JJBlancoDev <janerjoseblancomartinez@gmail.com>
2025-04-19 22:17:20 -0500

    Implementando metodos findAll, findOne, remove

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,12 +1,23 @@
-import { Injectable, InternalServerErrorException } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  InternalServerErrorException,
+  Logger,
+  NotFoundException,
+} from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Product } from './entities/product.entity';
+import { PaginationDto } from 'src/common/dto/pagination.dto';
+
+import { validate as isUUID } from 'uuid';
 
 @Injectable()
 export class ProductsService {
+  private readonly logger = new Logger('ProductService');
+
   constructor(
     @InjectRepository(Product)
     private readonly productRepository: Repository<Product>,
@@ -19,24 +30,70 @@ export class ProductsService {
 
       return product;
     } catch (error) {
-      console.log(error);
-      throw new InternalServerErrorException('help');
+      this.handlerExceptions(error);
     }
   }
 
-  findAll() {
-    return `This action returns all products`;
+  async findAll(paginationDto: PaginationDto) {
+    const { limit = 10, page = 1 } = paginationDto;
+    const offset = (page - 1) * limit;
+
+    const [data, total] = await this.productRepository.findAndCount({
+      take: limit,
+      skip: offset,
+      // TODO: Relaciones
+    });
+
+    const totalPages = Math.ceil(total / limit);
+
+    return {
+      pagination: {
+        page,
+        total,
+        limit,
+        pages: totalPages,
+      },
+      data,
+    };
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} product`;
+  async findOne(term: string) {
+    let conditionSearch: object = { id: term };
+
+    if (!isUUID(term)) {
+      conditionSearch = { slug: term.toLowerCase() };
+    }
+
+    const product = await this.productRepository.findOneBy(conditionSearch);
+
+    if (!product) {
+      throw new NotFoundException(`Product with ${term} not found`);
+    }
+
+    return product;
   }
 
   update(id: number, updateProductDto: UpdateProductDto) {
     return `This action updates a #${id} product`;
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} product`;
+  async remove(id: string) {
+    const product = await this.findOne(id);
+    await this.productRepository.remove(product);
+
+    return {
+      message: `Product with id ${id} deleted`,
+    };
+  }
+
+  private handlerExceptions(error: any) {
+    if (error.code === '23505') {
+      throw new BadRequestException(error.detail);
+    }
+
+    this.logger.error(error);
+    throw new InternalServerErrorException(
+      'Unexpected error, check server logs.',
+    );
   }
 }
